6caaad49e5633a4738c745e7b49eccee00ea4f4d
hibo98 <niklasmerkelt@mail.de>
2019-09-30 10:12:20 +0200

    Go to page, when filters are changed

--- src/Anura/DataDrivenTables/resources/FilterableTable.js
+++ src/Anura/DataDrivenTables/resources/FilterableTable.js
@@ -5,6 +5,7 @@ function setupFilterable(tableId) {
     var filters_select = filters.querySelectorAll("select");
     for (var i = 0; i < filters_input.length; i++) {
         filters_input[i].addEventListener("input", function () {
+            table.dataset.page = 1;
             var obj = JSON.parse(table.dataset.additionalParameters);
             if (this.type === "checkbox") {
                 var parent = this.parentElement;
@@ -22,6 +23,7 @@ function setupFilterable(tableId) {
     }
     for (var i = 0; i < filters_select.length; i++) {
         filters_select[i].addEventListener("change", function () {
+            table.dataset.page = 1;
             var obj = JSON.parse(table.dataset.additionalParameters);
             obj["tableFilter-" + this.dataset.id] = this.value;
             table.dataset.additionalParameters = JSON.stringify(obj);
